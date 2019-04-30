3566d9dc1f15734acef5ff225d1c116fd33ec708
CLJunge <j.chris_lennart@ymail.com>
2019-04-30 11:30:57 +0200

    Added missing regions to IInputHandler

--- Spawn.InputOverlay/Input/IInputHandler.cs
+++ Spawn.InputOverlay/Input/IInputHandler.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿#region Using
+using System;
+#endregion
 
 namespace Spawn.InputOverlay.Input
 {
     public interface IInputHandler : IDisposable
     {
+        #region EventHandlers
         event EventHandler DeviceConnected;
         event EventHandler DeviceDisconnected;
         event EventHandler<InputUpdatedEventArgs> InputUpdated;
+        #endregion
 
+        #region Properties
         bool IsDeviceConnected { get; }
+        #endregion
 
+        #region Methods
         void RestartTimer();
+        #endregion
     }
 }

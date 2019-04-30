f32fad16d8727890cec3d3b3984a4dc0800bf301
CLJunge <j.chris_lennart@ymail.com>
2019-04-30 13:13:28 +0200

    Adjust log level

--- Spawn.InputOverlay/UI/ViewModels/OverlayViewModel.cs
+++ Spawn.InputOverlay/UI/ViewModels/OverlayViewModel.cs
@@ -303,7 +303,7 @@ namespace Spawn.InputOverlay.UI.ViewModels
             m_inputHandler.DeviceDisconnected += OnDeviceDisconnected;
             m_inputHandler.InputUpdated += OnInputUpdated;
 
-            s_logger.Info("Using {0}", m_inputHandler.GetType().Name);
+            s_logger.Debug("Using {0}", m_inputHandler.GetType().Name);
 
             LoadValues();
         }

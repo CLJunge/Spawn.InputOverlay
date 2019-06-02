389a054585ca7401573ac50a880786665f26f6b5
CLJunge <j.chris_lennart@ymail.com>
2019-06-02 16:44:19 +0200

    Added FloatConverter

---
+++ Spawn.InputOverlay/UI/Converters/FloatConverter.cs
@@ -0,0 +1,35 @@
+﻿#region Using
+using System;
+using System.Globalization;
+using System.Windows.Data;
+#endregion
+
+namespace Spawn.InputOverlay.UI.Converters
+{
+    public class FloatConverter : IValueConverter
+    {
+        #region Convert
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strRet = value.ToString();
+
+            if (value is float fValue)
+                strRet = Math.Round(fValue, 3).ToString(CultureInfo.InvariantCulture);
+
+            return strRet;
+        }
+        #endregion
+
+        #region ConvertBack
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float fRet = 0f;
+
+            if (targetType.Equals(typeof(float)))
+                fRet = System.Convert.ToSingle(value);
+
+            return fRet;
+        }
+        #endregion
+    }
+}

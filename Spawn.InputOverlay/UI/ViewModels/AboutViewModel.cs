9132f7346cff5edbf4be404a7961b4f18bc2b971
CLJunge <j.chris_lennart@ymail.com>
2019-06-02 17:35:30 +0200

    Added AboutViewModel

---
+++ Spawn.InputOverlay/UI/ViewModels/AboutViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Spawn.InputOverlay.UI.ViewModels
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        #region Member Variables
+        private string m_strWindowTitle;
+        #endregion
+
+        #region Properties
+        #region WindowTitle
+        public string WindowTitle
+        {
+            get => m_strWindowTitle;
+            set => Set(ref m_strWindowTitle, value);
+        }
+        #endregion
+        #endregion
+
+        #region MyRegion
+        public AboutViewModel() => Initialize();
+        #endregion
+
+        #region Initialize
+        private void Initialize()
+        {
+            WindowTitle = $"{App.AppName} - About";
+        }
+        #endregion
+    }
+}

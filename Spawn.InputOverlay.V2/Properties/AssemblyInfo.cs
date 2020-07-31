7d741351ac29a7a3decf665e3f6bcc5a8636e37e
Chris-Lennart Junge <junge.cl@protonmail.com>
2020-07-31 18:47:01 +0200

    Set initial application version

--- Spawn.InputOverlay.V2/Properties/AssemblyInfo.cs
+++ Spawn.InputOverlay.V2/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -30,7 +28,6 @@ using System.Windows;
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
@@ -39,7 +36,6 @@ using System.Windows;
                                               //(used if a resource is not found in the page,
                                               // app, or any theme specific resource dictionaries)
 )]
-
 
 // Version information for an assembly consists of the following four values:
 //
@@ -51,7 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
-
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2020.7.31.1")]
+[assembly: AssemblyFileVersion("2020.7.31.1")]
+[assembly: AssemblyInformationalVersion("2020.7.31")]

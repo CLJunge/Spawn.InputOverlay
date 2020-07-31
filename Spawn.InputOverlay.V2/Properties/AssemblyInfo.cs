05327d749917608dce569a9074f40d6596eea418
Chris-Lennart Junge <junge.cl@protonmail.com>
2020-07-31 18:49:55 +0200

    Added placeholder icon

--- Spawn.InputOverlay.V2/Properties/AssemblyInfo.cs
+++ Spawn.InputOverlay.V2/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.7.31.2")]
-[assembly: AssemblyFileVersion("2020.7.31.2")]
+[assembly: AssemblyVersion("2020.7.31.3")]
+[assembly: AssemblyFileVersion("2020.7.31.3")]
 [assembly: AssemblyInformationalVersion("2020.7.31")]

ee1b513f07237e159a8c822ba63a69fa0aa043ad
Chris-Lennart Junge <junge.cl@protonmail.com>
2020-07-31 18:58:13 +0200

    Added basic MVVM support

---
+++ Spawn.InputOverlay.V2/UI/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+#endregion
+
+namespace Spawn.InputOverlay.UI
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        #region EventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent([CallerMemberName] string strPropertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(strPropertyName));
+        #endregion
+
+        #region Set
+        protected void Set<T>(ref T field, T newValue, [CallerMemberName] string strPropertyName = "")
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                field = newValue;
+
+                RaisePropertyChangedEvent(strPropertyName);
+            }
+        }
+        #endregion
+
+        #region Dispose
+        public virtual void Dispose()
+        {
+        }
+        #endregion
+    }
+}
